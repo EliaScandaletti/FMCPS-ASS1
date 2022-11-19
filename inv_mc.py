11c5e1a79048a2eb8e3197ad962e1b34d512fe13
Elia Scandaletti <elia.scandaletti@gmail.com>
2022-11-19 12:54:42 +0100

    Trace does work

--- inv_mc.py
+++ inv_mc.py
@@ -1,5 +1,7 @@
+from typing import List
 import pynusmv
 import sys
+
 
 def spec_to_bdd(model, spec):
     """
@@ -7,6 +9,28 @@ def spec_to_bdd(model, spec):
     """
     bddspec = pynusmv.mc.eval_ctl_spec(model, spec)
     return bddspec
+
+
+def create_trace(model: pynusmv.fsm.BddFsm, sym_trace: List[pynusmv.dd.BDD]) -> List[pynusmv.dd.State]:
+    if len(sym_trace) == 0:
+        return []
+
+    if len(sym_trace) == 1:
+        return [model.pick_one_state(sym_trace[0])]
+
+    sym_target = sym_trace.pop()
+    target = model.pick_one_state(sym_target)
+    # print(f'picked: {target.get_str_values()}')
+
+    sym_trace[-1] = sym_trace[-1] & model.pre(target)
+    # if sym_trace[-1].is_false():
+    #     for s in model.pick_all_states(model.pre(target)):
+    #         print(s.get_str_values())
+
+    trace = create_trace(model, sym_trace)
+    trace.append(target)
+    return trace
+
 
 def check_explain_inv_spec(spec):
     """
@@ -24,7 +48,7 @@ def check_explain_inv_spec(spec):
     and ending with a state. States and inputs are represented by dictionaries
     where keys are state and inputs variable of the loaded SMV model, and values
     are their value.
-    
+
     function SymbolicReachable(Init, Trans, Inv)
         Reach ← Init
         New ← Init
@@ -39,7 +63,6 @@ def check_explain_inv_spec(spec):
     end function
     """
 
-
     # a + b and a | b compute the disjunction of a and b
     # a * b and a & b compute the conjunction of a and b
     # ~a and -a compute the negation of a
@@ -47,23 +70,31 @@ def check_explain_inv_spec(spec):
     # a ^ b computes the exclusive-OR (XOR) of a and b
     # a == b, a <= b, a < b, a > b and a >= b compare a and b
 
-
     model = pynusmv.glob.prop_database().master.bddFsm
     invariant = spec_to_bdd(model, spec)
     not_invarinat = ~invariant
 
     reach = model.init
     new = model.init
-    trace = []
+    sym_trace = [new]
 
     while not new.is_false():
         if new.intersected(not_invarinat):
-            # trace = ...
+            sym_trace[-1] = sym_trace[-1] & not_invarinat
+            # for bdd in sym_trace:
+            # print('---')
+            # for s in model.pick_all_states(bdd):
+            #     print(s.get_str_values())
+            trace = create_trace(model, sym_trace)
+            
+            for state in trace:
+                print(state.get_str_values())
+
             return False, trace
         new = model.post(new) - reach
+        sym_trace.append(new)
         reach = reach + new
     return True, None
-
 
     # for state in model.pick_all_states(invariant):
     #     print(state.get_str_values())
